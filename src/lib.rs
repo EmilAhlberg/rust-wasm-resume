454af0bd4b0bb97407ed57dde37108babe4ead7a
Emil Ahlberg <emil.ahlberg1@gmail.com>
2022-10-30 15:51:16 +0100

    yaml deserialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 mod utils;
 
+//use indoc::indoc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Response, Window};
+use serde_yaml::Mapping;
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,33 +16,69 @@ use web_sys::{Request, RequestInit, RequestMode, Response, Window};
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
+// Next let's define a macro that's like `println!`, only it works for
+// `console.log`. Note that `println!` doesn't actually work on the wasm target
+// because the standard library currently just eats all output. To get
+// `println!`-like behavior in your app you'll likely want a macro like this.
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
 }
+
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    run();
-    //alert(format!("Hello {}", name).as_str());
-}
-
-
-#[wasm_bindgen]
-pub struct Resume {
-    width: u32,
-    height: u32,
-    content: String
-}
-
-use std::fmt;
-
-impl fmt::Display for Resume {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", "symbol")?;
-        write!(f, "\n")?;
-        Ok(())
+pub async fn greet(name: &str) {
+    console_log!("Let's print some numbers...");
+    //log("Hello from Rust!");
+    let yaml = run().await;
+    match yaml {
+        Ok(v) => buildResume(v),
+        Err(e) => println!("error {e:?}"),
     }
+    alert(format!("Hello {}", name).as_str());
+}
+
+fn buildResume(s:String) {
+    console_log!("buildresume {}",s);
+    let t = read(s);
+    match t {
+        Ok(v) => console_log!("ok"),
+        Err(e) => console_log!("not ok {}", e),
+    }
+    //Ok(())
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Node {
+    supported_by : Option<Vec<String>>,
+    #[serde(flatten)]
+    extras: HashMap<String, String>,
+}
+
+fn read(s: String)->  Result<(), serde_yaml::Error> {
+    console_log!("hej{}",s);
+    let deser: Vec<Node> = serde_yaml::from_str(&s)?;
+    println!("{:#?}", deser);
+    Ok(())
 }
 
 #[wasm_bindgen]
@@ -55,7 +96,7 @@ pub fn add_heading() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen]
-pub async fn run() -> Result<JsValue, JsValue> {
+pub async fn run() -> Result<String, JsValue> {
     let mut opts = RequestInit::new();
     opts.method("GET");
     opts.mode(RequestMode::Cors);
@@ -76,8 +117,8 @@ pub async fn run() -> Result<JsValue, JsValue> {
     let resp: Response = resp_value.dyn_into().unwrap();
 
     // Convert this other `Promise` into a rust `Future`.
-    let json = JsFuture::from(resp.json()?).await?;
+    let yamlText = JsFuture::from(resp.text()?).await?.as_string().unwrap();
 
     // Send the JSON response back to JS.
-    Ok(json)
+    Ok(yamlText)
 }
